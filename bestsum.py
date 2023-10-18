9542d68b185a47a5e2a749091c4673fa510f7fb1
Thomas Hamer <thomas_hamer@outlook.com>
2023-10-19 00:10:23 +1100

    bestsum

--- bestsum.py
+++ bestsum.py
@@ -9,8 +9,14 @@ def bestSum(targetSum, numbers):
         remainder = targetSum - num
         remainderResult = bestSum(remainder, numbers)
         if remainderResult != None:
-            return remainderResult + [num]
-        if shortest == None or len(remainderResult) < len(shortest):
-            shortest = remainderResult + [num]
+            combination = remainderResult + [num]
+            if shortest == None or len(combination) < len(shortest):
+                shortest = combination
     
     return shortest
+
+print(bestSum(7, [5, 3, 4, 7])) # [7]
+print(bestSum(8, [2, 3, 5])) # [3, 5]
+print(bestSum(8, [1, 4, 5])) # [4, 4]
+print(bestSum(100, [1, 2, 5, 25])) # [25, 25, 25, 25] # will take ages to run
+print(bestSum(300, [7, 14])) # None # will never have the time to wait for this to finish
